35bb0196b7bdacd20c69be821bdea93635d50fea
staniszevskyy <expunge96@hotmail.com>
2018-06-19 22:12:28 +0200

    Changing params

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,9 +13,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Scanner x = new Scanner(System.in);
-        String path;
+
         System.out.println("Podaj ścieżkę do folderu z którego mają być usunięte duplikaty zdjęć");
-        path=x.nextLine();
+        String path=x.nextLine();
+
         deleteFromFolder del = new deleteFromFolder(path);
         del.delete();
 
